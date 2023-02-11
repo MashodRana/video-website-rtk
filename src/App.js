1471a4d2a498788510e5db7a6b7c23bba524bf54
MashodRana <mashod0rana@gmail.com>
2023-02-12 00:15:51 +0600

    video page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import Home from './pages/Home';
+import Video from './pages/Video';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <Video />
     </div>
   );
 }
---
+++ src/pages/Video.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import VideoDescription from '../components/description/VideoDescription'
+import VideoPlayer from '../components/description/VideoPlayer'
+import Footer from '../components/Footer'
+import RelatedVideoList from '../components/list/RelatedVideoList'
+import Navbar from '../components/navbar/Navbar'
+
+export default function Video() {
+    return (
+        <>
+            <Navbar />
+            <section className="pt-6 pb-20">
+                <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+                    <div className="grid grid-cols-3 gap-2 lg:gap-8">
+                        <div className="col-span-full w-full space-y-8 lg:col-span-2">
+                            {/* <!-- video player --> */}
+                            <VideoPlayer />
+
+                            {/* <!-- video description --> */}
+                            <VideoDescription />
+                        </div>
+
+                        {/* <!-- related videos --> */}
+                        <RelatedVideoList />
+
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
+}
